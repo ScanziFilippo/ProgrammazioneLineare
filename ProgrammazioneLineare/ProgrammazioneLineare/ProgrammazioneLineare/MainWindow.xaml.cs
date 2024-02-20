287ef95ba35a7d63031d90802f7540c140378e64
ScanziFilippo <scanzi.filippo.studente@itispaleocapa.it>
2024-02-20 12:00:39 +0100

    0.01

---
+++ ProgrammazioneLineare/ProgrammazioneLineare/ProgrammazioneLineare/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using BlurryControls.Controls;
+using BlurryControls.DialogFactory;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProgrammazioneLineare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BlurryMessageBox.Show(this, "Contenuto contenutoso", "Titolo");
+        }
+    }
+}
