4a36f1f9e7e80ea0109c68dbd437fc84ef3a16bf
ScanziFilippo <101709401+ScanziFilippo@users.noreply.github.com>
2024-05-14 10:25:46 +0200

    1.0
    .Net 6.0 final version added

--- ProgrammazioneLineare/ProgrammazioneLineare/obj/Debug/net5.0-windows/App.g.i.cs
+++ ProgrammazioneLineare/ProgrammazioneLineare/obj/Debug/net5.0-windows/App.g.i.cs
@@ -48,7 +48,7 @@ namespace ProgrammazioneLineare {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -60,7 +60,7 @@ namespace ProgrammazioneLineare {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/ProgrammazioneLineare;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ProgrammazioneLineare;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -74,7 +74,7 @@ namespace ProgrammazioneLineare {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public static void Main() {
             ProgrammazioneLineare.App app = new ProgrammazioneLineare.App();
             app.InitializeComponent();
